d5b77d74e9a1db91534ae3eb46ccefbe0d8fcb6b
Sofia Ex <sophia.exarchou@icloud.com>
2024-09-23 19:56:14 +0200

    hide and reveal diary card on click

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { Button } from "./Button";
 import { DiaryEntryCard } from "./Container";
+import { useState } from "react";
 
 function App() {
+    const [isDiaryEnryCardHidden, setIsDiaryEnryCardHidden] = useState(true);
+
     return (
         <div>
             <header />
@@ -16,22 +19,16 @@ function App() {
             >
                 <Button
                     title="Open Sample of Diary Entry"
-                    onClicked={() => {
-                        console.log("Open Sample of Diary Entry");
-                    }}
+                    onClicked={() => setIsDiaryEnryCardHidden(false)}
                 />
 
                 <DiaryEntryCard
                     title="I am a modal"
-                    isHidden={false}
+                    isHidden={isDiaryEnryCardHidden}
                     entryDate={new Date()}
                     imageUrl="/assets/image1.jpg"
                     entryContent="Sunt officia do minim et nostrud dolore nulla ut sint quis qui est. Dolor id do nisi ea nulla aliquip deserunt voluptate non. Culpa et cupidatat adipisicing consequat enim sunt commodo proident ex est ad consectetur quis sit. Ea Lorem occaecat proident nisi ullamco laborum. Adipisicing do duis qui aliqua nulla deserunt exercitation cillum anim laboris reprehenderit incididunt cupidatat consectetur. Do duis laboris velit elit deserunt. Exercitation ad ullamco ullamco fugiat ut elit incididunt id proident do ut."
-                    onCloseClicked={() => {
-                        console.log(
-                            "close button on diary entry card clicked."
-                        );
-                    }}
+                    onCloseClicked={() => setIsDiaryEnryCardHidden(true)}
                 />
             </main>
 
