c14f87c8c641728bd95d95b4782108a3421e2e06
Sofia Ex <sophia.exarchou@icloud.com>
2024-09-23 19:46:42 +0200

    add hidden to modal and close button

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,6 @@ function App() {
                     padding: "100px",
                 }}
             >
-                <Button title="HI" />
-
                 <Button
                     title="Open Sample of Diary Entry"
                     onClicked={() => {
@@ -25,9 +23,15 @@ function App() {
 
                 <DiaryEntryCard
                     title="I am a modal"
+                    isHidden={false}
                     entryDate={new Date()}
                     imageUrl="/assets/image1.jpg"
                     entryContent="Sunt officia do minim et nostrud dolore nulla ut sint quis qui est. Dolor id do nisi ea nulla aliquip deserunt voluptate non. Culpa et cupidatat adipisicing consequat enim sunt commodo proident ex est ad consectetur quis sit. Ea Lorem occaecat proident nisi ullamco laborum. Adipisicing do duis qui aliqua nulla deserunt exercitation cillum anim laboris reprehenderit incididunt cupidatat consectetur. Do duis laboris velit elit deserunt. Exercitation ad ullamco ullamco fugiat ut elit incididunt id proident do ut."
+                    onCloseClicked={() => {
+                        console.log(
+                            "close button on diary entry card clicked."
+                        );
+                    }}
                 />
             </main>
 
--- src/Container.jsx
+++ src/Container.jsx
@@ -1,13 +1,16 @@
 /* eslint-disable react/prop-types */
+import { Button } from "./Button";
 
 export const DiaryEntryCard = (props) => (
-    <div className=" p-4 border-cyan-600 border-4 rounded-md  bg-yellow-50">
+    <div
+        className={`${
+            props.isHidden ? "hidden" : ""
+        } p-4 border-cyan-600 border-4 rounded-md  bg-amber-300`}
+    >
         <h1>{props.title}</h1>
-
         <h2>{props.entryDate.toLocaleDateString("de-DE")}</h2>
-
         <img src={props.imageUrl} className="object-contain rounded-md p-3" />
-
         <p>{props.entryContent}</p>
+        <Button title="Close" onClicked={props.onCloseClicked} />
     </div>
 );
