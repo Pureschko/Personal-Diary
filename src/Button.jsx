3cb88f49d8ccc39d4b2e5dce2ca084ba796e21e1
Sofia Ex <sophia.exarchou@icloud.com>
2024-09-25 20:45:50 +0200

    replace diarydata with get item by key and use id for local storage

--- src/Button.jsx
+++ src/Button.jsx
@@ -3,6 +3,7 @@
 export const Button = props => (
     <button
         type="button"
+        style={props.style}
         className=" bg-cyan-500 hover:bg-amber-400 text-white p-2 m-2 max-w-max rounded-md"
         onClick={props.onClicked}
     >
